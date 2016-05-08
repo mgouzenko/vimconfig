2ffdf4b0d57462f4f97243ecb48715d5d300ecb1
Mitchell Gouzenko <mag2272@columbia.edu>
2016-05-08 01:50:23 -0400

    added more installation scripts

---
+++ blast_vimconfigs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf ~/.vim
+rm -f ~/.vimrc
--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
+mkdir ~/.vim
+cp -r ./* ~/.vim
+echo "runtime vimrc" > ~/.vimrc
+
 # Vundle is a package manager, used to install packages to .vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install all plugins
+vim -C "PluginInstall"
